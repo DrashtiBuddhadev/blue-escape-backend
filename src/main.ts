c393b58f1fda8b2e1cca57f761e0235135174f7a
DrashtiBuddhadev <buddhadevdrashti@gmail.com>
2025-10-09 11:15:12 +0530

    external request support added

--- src/main.ts
+++ src/main.ts
@@ -66,7 +66,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 3000;
-  await app.listen(port, 'localhost');
+  await app.listen(port, '0.0.0.0');
   console.log(`Application is running on: http://localhost:${port}`);
   console.log(`Swagger UI available at: http://localhost:${port}/api/docs`);
   console.log(`API JSON for admin panel: http://localhost:${port}/api/docs-json`);
